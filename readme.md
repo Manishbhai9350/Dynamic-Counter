6092d41ccd62820d5144e4d334dbc23aea782847
Manish <keshraram294@gmail.com>
2025-02-19 16:40:52 +0530

    Dynamic Counter

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Dynamic Counter
+
+( <a style='color:skyblue' href='https://text-grid-shader.onrender.com/' >View Live</a> )
+
+A dynamic counter that provides an engaging and interactive experience with real-time updates and smooth animations.
+
+Preview 
+![Gridy Shader Effect](./Counter.gif)
+
+# 💻 Tech Stack:
+[![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)](https://developer.mozilla.org/en-US/docs/Web/HTML) [![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)](https://developer.mozilla.org/en-US/docs/Web/CSS) [![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)](https://developer.mozilla.org/en-US/docs/Web/JavaScript) [![GSAP](https://img.shields.io/badge/GSAP-%2383C3D0.svg?style=for-the-badge&logo=greensock&logoColor=black)](https://greensock.com/gsap/)
+
+
+## About
+This project features a dynamic counter that offers an engaging and interactive experience with real-time updates and smooth animations. It leverages powerful libraries like GSAP for fluid motion and provides a visually appealing interface, showcasing the effective use of modern web technologies to enhance user interaction. 🚀
---
+++ script.js
@@ -0,0 +1,85 @@
+const Counter = document.querySelector('.counter')
+const Boxes = document.querySelectorAll(".box");
+const IncrementButton = document.querySelector('.increment.button')
+const DcrementButton = document.querySelector('.dcrement.button')
+const IncrementFactorInp = document.querySelector('.increment-factor-input')
+const DecrementFactorInp = document.querySelector('.decrement-factor-input')
+
+// IncrementFactorInp.setAttribute('minlength',0)
+// IncrementFactorInp.setAttribute('maxlength',Boxes.length)
+// DecrementFactorInp.setAttribute('minlength',0)
+// DecrementFactorInp.setAttribute('maxlength',Boxes.length)
+
+Boxes.forEach((Box, n) => {
+  CreateNums(Box, n);
+});
+function CreateNums(Box, n) {
+  Box.innerHTML = "";
+  const Nums = [];
+  const NumsCon = document.createElement("div");
+  NumsCon.classList.add("nums");
+  Box.setAttribute("box-i", n);
+
+  for (let i = 0; i < 10; i++) {
+    const Num = document.createElement("div");
+    Num.classList.add("num");
+    Num.setAttribute("num-i", i);
+    Num.innerHTML = `<p>${i}</p>`;
+    Nums.push(Num);
+  }
+  NumsCon.append(...Nums);
+  Box.appendChild(NumsCon);
+}
+
+let Count = 0;
+let Factors = {
+  increment:Number(IncrementFactorInp.value),
+  decrement:Number(DecrementFactorInp.value)
+}
+
+function UpdateCounter(n = 0) {
+  const PadNum = n.toString().padStart(Boxes.length, "0");
+  Counter.setAttribute('count',PadNum)
+  PadNum.split("").forEach((Num, i) => {
+    const number = Number(Num);
+    const Box = Boxes[i]
+    if(!Box) return;
+    const BoxNumsCon = Box.querySelector('.nums')
+    const BoxBounds = BoxNumsCon.querySelector('.num').getBoundingClientRect().height
+    const Translation = BoxBounds * number
+    gsap.to(BoxNumsCon,{
+        y:-Translation,
+        duration:(Boxes.length - i) * .2 + .5,
+        overwrite:true
+    })
+  });
+}
+
+UpdateCounter(Count);
+
+IncrementFactorInp.addEventListener('input', e => {
+  if(e.target.value.length > Boxes.length) {
+    e.target.value = Factors.increment
+  } else {
+    Factors.increment = Number(e.target.value)
+  }
+})
+DecrementFactorInp.addEventListener('input', e => {
+  if(e.target.value.length > Boxes.length) {
+    e.target.value = Factors.decrement
+  } else {
+    Factors.decrement = Number(e.target.value)
+  }
+})
+
+IncrementButton.addEventListener('click',e => {
+    Count += Factors.increment
+    UpdateCounter(Count)
+})
+DcrementButton.addEventListener('click',e => {
+    Count -= Factors.decrement
+    Count = Count < 0 ? 0 : Count
+    UpdateCounter(Count)
+})
+
+window.onresize = () => UpdateCounter(Count)
